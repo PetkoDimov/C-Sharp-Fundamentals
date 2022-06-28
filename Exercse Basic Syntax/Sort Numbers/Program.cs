26444a9a4e3a38287e429d8dfa678dc81960eea6
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:08:57 +0300

    Add files via upload

---
+++ Exercse Basic Syntax/Sort Numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thurdNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNumber && firstNumber > thurdNumber)
+            {
+                Console.WriteLine(firstNumber);
+                if (secondNumber > thurdNumber)
+                {
+                    Console.WriteLine(secondNumber);
+                    Console.WriteLine(thurdNumber);
+                }
+                else
+                {
+                    Console.WriteLine(thurdNumber);
+                    Console.WriteLine(secondNumber);
+                }
+
+            }
+            else if (secondNumber > firstNumber && secondNumber > thurdNumber)
+            {
+                Console.WriteLine(secondNumber);
+                if (firstNumber > thurdNumber)
+                {
+                    Console.WriteLine(firstNumber);
+                    Console.WriteLine(thurdNumber);
+                }
+                else
+                {
+                    Console.WriteLine(thurdNumber);
+                    Console.WriteLine(firstNumber);
+                }
+            }
+            else if (thurdNumber > firstNumber && thurdNumber > secondNumber)
+            {
+                Console.WriteLine(thurdNumber);
+                if (firstNumber > secondNumber)
+                {
+                    Console.WriteLine(firstNumber);
+                    Console.WriteLine(secondNumber);
+                }
+                else
+                {
+                    Console.WriteLine(secondNumber);
+                    Console.WriteLine(firstNumber);
+                }
+            }
+        }
+    }
+}
---
+++ Exercse Basic Syntax/Summ Of Odd Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Summ_Of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfOdds = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= NumberOfOdds; i++)
+            {
+                Console.WriteLine(2*i-1);
+                sum = sum + 2 * i - 1;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}

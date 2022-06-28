cbc840c15dd46256a2bd3f7255aaa1e2adccb120
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:29:31 +0300

    Add files via upload

---
+++ Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+
+namespace MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arr = Console.ReadLine().Split();
+            int bestCounter = 0;
+            int bestIndex = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string currentElement = arr[i];
+                int currentCounter = 1;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+
+                    if (currentElement == arr[j])
+                    {
+                        currentCounter++;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+                if (currentCounter > bestCounter)
+                {
+                    bestCounter = currentCounter;
+                    bestIndex = i;
+                }
+            }
+
+            for (int i = 0; i < bestCounter; i++)
+            {
+                Console.Write(arr[bestIndex] + " ");
+            }
+
+        }
+    }
+}
---
+++ Arrays/NumbersInReverseOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", arr[i]);
+            } //ili Console.Write(string.Join(' ', arr.Reverse()));
+        }
+    }
+}

ccdbe05af9a7e24c9af9aeaaeb83e72ade5ad3ed
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:59:28 +0300

    Add files via upload

---
+++ Exam/Problem/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            string command = Console.ReadLine();
+            
+
+            while (command != "End")
+            {
+                string[] tokens = command.Split(" ");
+                string cmdArg = tokens[0];
+
+                if (cmdArg == "Translate")
+                {
+                    string charachter = tokens[1];
+                    string replacement = tokens[2];
+                    input = input.Replace(charachter, replacement);
+                    Console.WriteLine(input);
+
+                }
+                else if (cmdArg == "Includes")
+                {
+                    string contains = tokens[1];
+                    if (input.Contains(contains))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+
+                }
+                else if (cmdArg == "Start")
+                {
+                    string start = tokens[1];
+                    if (input.StartsWith(start))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (cmdArg == "Lowercase")
+                {
+                    input = input.ToLower();
+                    Console.WriteLine(input);
+                }
+                else if (cmdArg == "FindIndex")
+                {
+                    string character = tokens[1];
+                    Console.WriteLine(input.LastIndexOf(character));
+
+                }
+                else if (cmdArg == "Remove")
+                {
+                    int startIndex = int.Parse(tokens[1]);
+                    int count = int.Parse(tokens[2]);
+
+                    input = input.Remove(startIndex, count);
+                    Console.WriteLine(input);
+
+                }
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exam/Shoping List/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shoping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shopingList = Console.ReadLine()
+                .Split("!")
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Go Shopping!")
+            {
+                string[] command = input.Split();
+                string cmdArg = command[0];
+                string product = command[1];
+
+
+                if (cmdArg == "Urgent" && !shopingList.Contains(product))
+                {
+
+
+                    shopingList.Insert(0, product);
+
+                }
+
+                else if (cmdArg == "Unnecessary")
+                {
+                    shopingList.Remove(product);
+                }
+
+                else if (cmdArg == "Correct" && shopingList.Contains(product))
+                {
+                    string newItem = command[2];
+
+
+                    int indexOfProduct = shopingList.IndexOf(product);
+                    shopingList.RemoveAt(indexOfProduct);
+                    shopingList.Insert(indexOfProduct, newItem);
+
+                }
+
+                else if (cmdArg == "Rearrange" && shopingList.Contains(product))
+                {
+
+                    shopingList.Remove(product);
+                    shopingList.Add(product);
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", shopingList));
+        }
+    }
+}

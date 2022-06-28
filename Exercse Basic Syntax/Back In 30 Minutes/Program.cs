fe5ff4dc4c341d176f323d7c2b2b80c983a2a42d
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:01:29 +0300

    Add files via upload

---
+++ Exercse Basic Syntax/Back In 30 Minutes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+
+            double minutes = double.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes >= 60 )
+            {
+                hour = hour + 1;
+                minutes = minutes - 60;
+            }
+
+            if (hour == 24)
+            {
+                hour = 0;
+            }
+
+            if (minutes < 10)
+            {
+                Console.WriteLine($"{hour}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{minutes}");
+            }
+        }
+    }
+}
---
+++ Exercse Basic Syntax/Divided By 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Divided_By_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 100;
+
+            for (int i = 3; i < input; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

cca9ab12df67a24727b0cf376812363a5a7f7041
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:37:16 +0300

    Add files via upload

---
+++ Associative Arrays/A Miner Task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            string resourses = Console.ReadLine();
+
+            while (resourses != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!output.ContainsKey(resourses))
+                {
+
+                    output.Add(resourses, 0);
+
+                }
+                output[resourses] += quantity;
+
+                resourses = Console.ReadLine();
+            }
+
+            foreach (var item in output)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+            
+        }
+    }
+}

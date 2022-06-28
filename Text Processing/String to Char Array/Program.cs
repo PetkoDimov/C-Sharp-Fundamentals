09b202489486fe75010257e766104ffbfff88e26
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:50:27 +0300

    Add files via upload

---
+++ Text Processing/String to Char Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace String_to_Char_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "Hello, C#";
+
+            Test(a.ToCharArray());
+
+        }
+
+
+        private static void Test(char[] arr)
+
+        {
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Text Processing/Text Filter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] filteredWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            foreach (var word in filteredWords)
+            {
+
+                string replace = new string('*', word.Length);
+
+                text = text.Replace(word, replace);
+
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}

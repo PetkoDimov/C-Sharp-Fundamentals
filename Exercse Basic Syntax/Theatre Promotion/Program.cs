663e72f6449391621b838b9a63927ac39ca13de0
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:10:31 +0300

    Add files via upload

---
+++ Exercse Basic Syntax/Theatre Promotion/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (age < 0 || age >123)
+            {
+                Console.WriteLine("Error!");
+            }
+
+            else if (typeOfDay == "Weekday")
+            {
+                if (age >= 0 && age <= 18  )
+                {
+                    price = 12;
+                }
+                else if (age > 18 && age <=64)
+                {
+                    price = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+                Console.WriteLine($"{price}$");
+
+            }
+
+            else if (typeOfDay == "Weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+
+                Console.WriteLine($"{price}$");
+
+            }
+
+            else if (typeOfDay == "Holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+
+                Console.WriteLine($"{price}$");
+
+            }
+
+        }
+    }
+}

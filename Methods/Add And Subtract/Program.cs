697eadb0f66b1f0594ac764dc16a606a40322476
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:20:58 +0300

    Add files via upload

---
+++ Methods/Add And Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Add_And_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thurdNumber = int.Parse(Console.ReadLine());
+
+            int sum = Sum(firstNumber, secondNumber, thurdNumber);
+
+            Console.WriteLine(sum);
+        }
+
+        private static int Sum(int firstNumber, int secondNumber, int thurdNumber)
+        {
+            int sumFirstAndSecont = firstNumber + secondNumber;
+            return Subtract(sumFirstAndSecont, thurdNumber);
+        }
+
+        private static int Subtract(int sumFirstAndSecont, int thurdNumber)
+        {
+            return sumFirstAndSecont - thurdNumber;
+        }
+    }
+}
---
+++ Methods/Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            PrintGrade(grade);
+        }
+
+        static void PrintGrade(double ocenka)
+        {
+
+            if (ocenka >= 2.00 && ocenka <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+
+            else if (ocenka >= 3.00 && ocenka <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+
+            else if (ocenka >= 3.50 && ocenka <= 4.49) 
+            {
+                Console.WriteLine("Good");
+            }
+
+            else if (ocenka >= 4.50 && ocenka <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (ocenka >= 5.50 && ocenka <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}

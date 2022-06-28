28e92ec2bfa14f1c8e9945bfc0d1d2719d312fef
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:34:34 +0300

    Add files via upload

---
+++ Arrays/Train/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{arr[j]} ");
+                sum = sum + arr[j];
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"{sum}");
+        }
+    }
+}

b4fdb5685bb2f3e262febc4aed6405e7724db981
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:22:18 +0300

    Add files via upload

---
+++ Arrays/Even And Odd Substraction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+            int result = 0;
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    sumEven = sumEven + array[i];
+                }
+                else 
+                {
+                    sumOdd += array[i];
+                }
+            }
+
+            result = sumEven - sumOdd;
+            Console.WriteLine(result);
+
+
+        }
+    }
+}

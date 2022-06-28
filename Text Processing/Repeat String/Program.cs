6122e37398cf6e713d959c9d529418d958fac628
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:48:23 +0300

    Add files via upload

---
+++ Text Processing/Repeat String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string result = string.Empty;
+
+            foreach (var word in input)
+            {
+
+                int length = word.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+                    result += word;
+                }
+
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Text Processing/String Builder Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+
+namespace String_Builder_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hello Peter, how are you?");
+
+            Console.WriteLine(sb[5]);
+
+            sb.Insert(11, " Ivanov");
+
+            Console.WriteLine(sb);
+
+            sb.Replace("Peter", "Mitko");
+
+            Console.WriteLine(sb);
+        }
+    }
+}

586053c9bc02cda994dbc1f97606b7b20be05cf7
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:47:36 +0300

    Add files via upload

---
+++ Data Type And Variables/WaterOverflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int amountinTank = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int waterToPour = int.Parse(Console.ReadLine());
+                bool isFull = waterToPour > 255;
+                bool isOverflowed = amountinTank + waterToPour > 255;
+
+                if (isFull || isOverflowed)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                        
+                }
+
+                amountinTank = amountinTank + waterToPour;
+            }
+
+            Console.WriteLine(amountinTank);
+        }
+    }
+}

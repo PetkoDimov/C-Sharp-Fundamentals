74016dfdcdf8e880424e469ef11e9a9b96bbac3d
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:36:55 +0300

    Add files via upload

---
+++ Text Processing/Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                var currentChar = ch + 3;
+                Console.Write((char)currentChar);
+            }
+
+        }
+    }
+}
---
+++ Text Processing/Character Multiplier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tokens = Console.ReadLine().Split(" ");
+
+            var firstWord = tokens[0];
+            var secondWord = tokens[1];
+
+            var longestWord = firstWord;
+            var shortestWord = secondWord;
+
+            if (firstWord.Length < secondWord.Length)
+            {
+                longestWord = secondWord;
+                shortestWord = firstWord;
+            }
+
+            var total = Character(longestWord, shortestWord);
+            Console.WriteLine(total);
+
+        }
+
+        public static int Character(string longestWord, string shortestWord)
+
+        {
+            var sum = 0;
+
+            for (int i = 0; i < shortestWord.Length; i++)
+            {
+                var multiply = longestWord[i] * shortestWord[i];
+                sum += multiply;
+            }
+
+            for (int i = shortestWord.Length; i < longestWord.Length; i++)
+            {
+                sum += longestWord[i];
+            }
+
+            return sum;
+        }
+    }
+}

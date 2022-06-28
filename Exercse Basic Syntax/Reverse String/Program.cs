171750ef746da783652e820f62b86d579380ff6e
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:05:38 +0300

    Add files via upload

---
+++ Exercse Basic Syntax/Reverse String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] cArray = input.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+
+            Console.WriteLine(reverse);
+        }
+
+    }
+}

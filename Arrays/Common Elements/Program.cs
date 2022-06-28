1d80d8a710c983972280dd6b0ac66fb21c56fec7
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:20:17 +0300

    Add files via upload

---
+++ Arrays/Common Elements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> firstList = Console.ReadLine().
+                Split(" ")
+                .ToList();
+            List<string> secondList = Console.ReadLine().
+                Split(" ")
+                .ToList();
+
+            foreach (var item in firstList)
+            {
+
+                if (secondList.Contains(item))
+                {
+                    Console.Write($"{item} ");
+                }
+
+            }
+
+
+        }
+    }
+}

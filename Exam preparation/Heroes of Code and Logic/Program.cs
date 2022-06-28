268fb1506c493f36117f3a9a9a4e5544073d8f95
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 17:49:52 +0300

    Add files via upload

---
+++ Exam preparation/Heroes of Code and Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Heroes_of_Code_and_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

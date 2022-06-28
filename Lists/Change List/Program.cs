002ae3c6ad9b1fec4e2bd6fd4baa1e9efe56d04a
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 18:15:51 +0300

    Add files via upload

---
+++ Lists/Change List/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInt = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] cmdArg = command.Split();
+                string firstCommand = cmdArg[0];
+                int element = int.Parse(cmdArg[1]);
+
+                if (firstCommand == "Delete")
+                {
+                    listOfInt.RemoveAll(x => x == element);
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int index = int.Parse(cmdArg[2]);
+                    listOfInt.Insert(index, element);
+                }
+
+                command = Console.ReadLine();
+
+            }
+
+            Console.WriteLine(string.Join(" ", listOfInt));
+
+        }
+    }
+}
